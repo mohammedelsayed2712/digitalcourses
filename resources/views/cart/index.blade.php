9c5bc758dafe802b573e28947862424626a41b86
mohammedelsayed2712 <mohammedelsayed2712@gmail.com>
2025-03-03 17:32:01 +0200

    edit style

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -12,7 +12,7 @@
                     @if ($cart && count($cart->courses) > 0)
                     @foreach ($cart->courses as $course)
 
-                    <div class="card">
+                    {{-- <div class="card">
                         <div class="p-6 text-gray-900">
                             <a href="{{ route('courses.show', $course) }}">
                                 <h5>{{ $course->name }}</h5>
@@ -21,6 +21,12 @@
                             <p>Price: {{ $course->price }}</p>
                             <a href="{{ route('addToCart', $course) }}" class="btn btn-sm btn-primary">Add To Cart</a>
                         </div>
+                    </div> --}}
+                    <div class="bg-light p-2 mb-3 d-flex justify-content-between align-items-center">
+                        <h6>{{ $course->name }}
+                            <small class="text-primary">({{ $course->price }})</small>
+                        </h6>
+                        <a href="#" class="btn btn-sm btn-danger">Remove</a>
                     </div>
 
                     @endforeach
