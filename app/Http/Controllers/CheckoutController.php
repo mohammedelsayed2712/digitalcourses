004dd0a9a6eb3aae21c8385503a4f1ecea6df9c0
mohammedelsayed2712 <mohammedelsayed2712@gmail.com>
2025-03-08 15:26:07 +0200

    setup orders

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cart;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class CheckoutController extends Controller
@@ -12,12 +13,38 @@ class CheckoutController extends Controller
         $prices = $cart->courses->pluck('stripe_price_id')->toArray();
 
         $sessionOptions = [
-            'success_url' => route('home', ['message' => 'payment success']),
+            // 'success_url' => route('home', ['message' => 'payment success']),
+            'success_url' => route('checkout.success') . '?session_id={CHECKOUT_SESSION_ID}',
             'cancel_url'  => route('home', ['message' => 'payment canceled']),
-            // 'billing_address_collection' => 'required',
-            // 'phone_number_collection'    => ['enabled' => true],
+            'metadata'    => [
+                'cart_id' => $cart->id,
+                'user_id' => Auth::user()->id,
+            ],
         ];
 
-        return Auth::user()->checkout($prices, $sessionOptions);
+        $customerOptions = [
+            'email' => Auth::user()->email,
+        ];
+
+        return Auth::user()->checkout($prices, $sessionOptions, $customerOptions);
+    }
+
+    public function success(Request $request)
+    {
+        // dd($request->get('session_id'));
+        $session = $request->user()->stripe()->checkout->sessions->retrieve($request->get('session_id'));
+        dd($session);
+
+        // $cart = Cart::find($session->metadata->cart_id);
+
+        // $cart->update([
+        //     'session_id' => $session->id,
+        //     'user_id'    => $session->metadata->user_id,
+        // ]);
+    }
+
+    public function cancel()
+    {
+        return view('checkout.cancel');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,8 @@ Route::controller(CartController::class)->group(function () {
 // Checkout
 Route::controller(CheckoutController::class)->group(function () {
     Route::get('/checkout', 'checkout')->middleware('auth')->name('checkout');
+    Route::get('/checkout/success', 'success')->middleware('auth')->name('checkout.success');
+    Route::get('/checkout/cancel', 'cancel')->middleware('auth')->name('checkout.cancel');
 });
 
 Route::get('/dashboard', function () {
