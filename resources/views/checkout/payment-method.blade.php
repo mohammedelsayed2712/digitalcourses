1dde189d92f38805b84f31f55c87cb5cd463673f
mohammedelsayed2712 <mohammedelsayed2712@gmail.com>
2025-03-15 19:09:43 +0200

    payment method p1

--- resources/views/checkout/payment-method.blade.php
+++ resources/views/checkout/payment-method.blade.php
@@ -9,9 +9,27 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{-- {{ __("You're logged in!") }} --}}
+                    <form action="#" method="POST">
+                        @csrf
+
+                        <!-- Stripe Elements Placeholder -->
+                        <div id="card-element"></div>
+
+                        <button id="card-button" class="btn btn-sm btn-primary mt-3">
+                            Process Payment
+                        </button>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
+
+    <script>
+        const stripe = Stripe('pk_test_51QwRjF2KxKhpsanMk3hRPVLC373m6wMoh1l7y0K8OwBstnD3ypbti77ZmIMsLVSzn1no6Ot9QLzRH1kPWz5Nxow500TAM4tbtL');
+     
+        const elements = stripe.elements();
+        const cardElement = elements.create('card');
+     
+        cardElement.mount('#card-element');
+    </script>
 </x-app-layout>
