03dddd356c7389ac013e4365a21c4f40166ed205
mohammedelsayed2712 <mohammedelsayed2712@gmail.com>
2025-03-18 17:52:46 +0200

    adds

--- app/Http/Controllers/PaymentMethodCheckoutController.php
+++ app/Http/Controllers/PaymentMethodCheckoutController.php
@@ -15,12 +15,17 @@ class PaymentMethodCheckoutController extends Controller
 
     public function post(Request $request)
     {
+        if ($request->payment_method) {
+            Auth::user()->addPaymentMethod($request->payment_method);
+        }
+
         $cart          = Cart::session()->first();
         $amount        = $cart->courses->sum('price');
         $paymentMethod = $request->payment_method;
         $payment       = Auth::user()->charge($amount, $paymentMethod, [
             'return_url' => route('home', ['message' => 'Payment successful!']),
         ]);
+
         if ($payment->status === 'succeeded') {
             $order = Order::create([
                 'user_id' => Auth::user()->id,
