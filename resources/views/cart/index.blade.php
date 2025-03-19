b04485c156464dcb9c51d672e3804fa4130ecf67
mohammedelsayed2712 <mohammedelsayed2712@gmail.com>
2025-03-19 19:16:30 +0200

    secure one click checkout

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -24,9 +24,11 @@
                             <small class="text-primary">({{ $cart->total() }})</small>
                         </h6>
                         <div>
+                            @if (Auth::user()->hasDefaultPaymentMethod())
                             <a href="{{ route('direct.paymentMethod.oneClick') }}" class="btn btn-sm btn-info">One
                                 Click
                                 Checkout</a>
+                            @endif
                             <a href="{{ route('direct.paymentMethod') }}" class="btn btn-sm btn-success">Checkout</a>
                         </div>
                     </div>
