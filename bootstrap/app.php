fe6f64c0dc75277906215c34ffad408d8b06ae88
mohammedelsayed2712 <mohammedelsayed2712@gmail.com>
2025-03-22 17:46:49 +0200

    setup cashier webhhook

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'protectOneClickCheckout' => protectOneClickCheckout::class,
         ]);
+        $middleware->validateCsrfTokens(except: [
+            'stripe/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
